8cb975babe6e8ce3ed9e2eaaa0001b78df547c50
18oreo <code.arindam189@gmail.com>
2026-08-17 20:54:24 +0530

    upgrade the music button work on progress

--- script1.js
+++ script1.js
@@ -1,3 +1,20 @@
-window.addEventListener('load',() =>{
-    document.getElementById('preloader').classList.add('fade-out')
+window.addEventListener('load', () => {
+    document.getElementById('preloader').classList.add('fade-out');
 });
+
+const musicButton = document.querySelector('.music-btn');
+const music = document.getElementById('background-music');
+const musicLabel = document.querySelector('.music-label');
+
+const musicBtn = document.getElementById('musicBtn')
+const bgMusic = document.getElementById('bgMusic')
+
+musicBtn.addEventListener("click",()=>{
+    if(bgMusic.pause){
+        bgMusic.play();
+        musicBtn.classList.add("playing")
+    } else {
+        bgMusic.pause();
+        musicBtn.classList.remove("playing")
+    }
+})
